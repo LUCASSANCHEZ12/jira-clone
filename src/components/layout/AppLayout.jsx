50ce9740c4b43347211504b807c89f2e82756001
LUCASSANCHEZ12 <lucassanchezsalinas@gmail.com>
2025-11-22 01:45:06 -0400

    added grid for sidebar

--- src/components/layout/AppLayout.jsx
+++ src/components/layout/AppLayout.jsx
@@ -1,4 +1,4 @@
-import { AppBar, Box, Button, Toolbar, Typography } from "@mui/material";
+import { AppBar, Box, Button, Grid, Toolbar, Typography } from "@mui/material";
 import { Outlet, Link, Navigate, useNavigate, NavLink } from "react-router-dom";
 
 import Header from "./Header";
@@ -16,8 +16,23 @@ export default function AppLayout() {
   return (
     <Box sx={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}>
       <Header />
-      <Content />
-      <Sidebar />
+      <Grid container sx={{ flexGrow: 1 }}>
+        <Grid item>
+          <Sidebar />
+        </Grid>
+        <Grid
+            xs
+            sx={{
+              backgroundColor: "#F4F5F7", // fondo estilo Jira
+              minHeight: "100vh",
+              p: 3,
+            }}
+        >
+          <Content />
+        </Grid>
+      
+      </Grid>
+      
     </Box>
   );
 }
